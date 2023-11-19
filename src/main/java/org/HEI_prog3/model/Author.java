3665264328c2508a0cc9273fd94df0165d40523e
RSitrakaFL <hei.sitraka@gmail.com>
2023-11-19 22:13:38 +0300

    add the Author entities

---
+++ src/main/java/org/HEI_prog3/model/Author.java
@@ -0,0 +1,15 @@
+package org.HEI_prog3.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+public class Author {
+    private int id;
+    private String name;
+    private char sex;
+}
